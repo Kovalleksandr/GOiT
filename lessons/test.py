9d443564fbd656054c54d22b6dd76652158e4f15
OLEKSANDR KOVAL <koval.aleksandr.v@gmail.com>
2025-03-04 19:29:19 +0200

    ok

--- lessons/test.py
+++ lessons/test.py
@@ -1,7 +1,23 @@
-def count_down(start):
-    while start > 0:
-        yield start
-        start -= 1
+class Pokemon:
+    def __init__(self, name, type, health):
+        self.name = name
+        self.type = type
+        self.health = health
 
-for number in count_down(5):
-    print(number)        
+    def attack(self, other_pokemon):
+        print(f"{self.name} attacks {other_pokemon.name}!")
+
+    def dodge(self):
+        print(f"{self.name} dodged the attack!")
+
+    def evolve(self, new_form):
+        print(f"{self.name} is evolving into {new_form}!")
+        self.name = new_form
+
+# Створення об'єкта Pikachu
+pikachu = Pokemon("Pikachu", "Electric", 100)
+
+# Використання методів
+pikachu.attack(Pokemon("Charmander", "Fire", 100))
+pikachu.dodge()
+pikachu.evolve("Raichu")
