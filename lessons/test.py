347ec23d7c9e1c15d433440e104ec6c605e3ad7e
OLEKSANDR KOVAL <koval.aleksandr.v@gmail.com>
2025-04-25 10:10:21 +0300

    re_connect

--- lessons/test.py
+++ lessons/test.py
@@ -1,23 +1,36 @@
-class Pokemon:
-    def __init__(self, name, type, health):
-        self.name = name
-        self.type = type
-        self.health = health
+import copy
 
-    def attack(self, other_pokemon):
-        print(f"{self.name} attacks {other_pokemon.name}!")
+class SimpleObject:
+    def __init__(self, greeting: str):
+        self.greeting = greeting
 
-    def dodge(self):
-        print(f"{self.name} dodged the attack!")
+class ComplexObject:
+    def __init__(self, value: int, nested_obj: SimpleObject):
+        self.value = value
+        self.nested_obj = nested_obj
 
-    def evolve(self, new_form):
-        print(f"{self.name} is evolving into {new_form}!")
-        self.name = new_form
+    def __copy__(self):
+        print("Викликано __copy__ для ComplexObject")
+        # Поверхневе копіювання не копіює вкладені об'єкти глибоко
+        return ComplexObject(self.value, self.nested_obj)
 
-# Створення об'єкта Pikachu
-pikachu = Pokemon("Pikachu", "Electric", 100)
+    def __deepcopy__(self, memo=None):
+        print("Викликано __deepcopy__ для ComplexObject")
+        # Глибоке копіювання копіює вкладені об'єкти
+        return ComplexObject(
+            copy.deepcopy(self.value, memo), copy.deepcopy(self.nested_obj, memo)
+        )
 
-# Використання методів
-pikachu.attack(Pokemon("Charmander", "Fire", 100))
-pikachu.dodge()
-pikachu.evolve("Raichu")
+nested_obj = SimpleObject("Привіт")
+complex_obj = ComplexObject(5, nested_obj)
+
+# Створюємо копію та глибоку копію
+complex_obj_copy = copy.copy(complex_obj)
+complex_obj_deepcopy = copy.deepcopy(complex_obj)
+
+# Змінюємо значення вкладеного об'єкту nested_obj
+nested_obj.greeting = "Hello"
+
+# Дивимось зміни у об'єктах
+print(f"Copy object: {complex_obj_copy.nested_obj.greeting}")
+print(f"Deepcopy object: {complex_obj_deepcopy.nested_obj.greeting}")
